5bca18c391c5da5d57ca1f51e2326e242538f5f8
yeswanth-xinthe <yeswanth.indana@xinthe.com>
2026-01-29 23:48:55 +0530

    Update Playwright config for new base URL

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://xactai.github.io/Vincense',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     /* Screenshot on failure */
@@ -35,12 +35,8 @@ export default defineConfig({
     /* Video on failure */
     video: 'on',
     headless: true,
-    viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
-    /* Action timeout */
-    actionTimeout: 15000,
-    /* Navigation timeout */
-    navigationTimeout: 30000,
+   
   },
 
   /* Configure projects for major browsers */
@@ -48,7 +44,7 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
+    }, 
 
     // {
     //   name: 'firefox',
@@ -82,11 +78,6 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:8080',
-    reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
-  },
+ 
 });
 
