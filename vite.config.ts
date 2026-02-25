6d409519547fbc6d05afc1420ab3e4604bc0c0ed
Xinthe-Sunaina-Naidu <sunaina.karri@xinthe.com>
2026-02-25 10:50:05 +0530

    Update Code with new repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/VinCense/",
+  base: "/communitiful-vincense/",
   plugins: [react()],
   server: {
     host: true, // Allows external access
